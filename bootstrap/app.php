99d4d3f92191f474ae1245c4af0aa9fa2657f334
Hafid <hafid@gmail.com>
2025-05-12 10:51:04 +0700

    feat : booking vehicle and new middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\AdminOrApproverMiddleware;
 use App\Http\Middleware\EnsureIsAdmin;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -15,6 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'isAdmin' => EnsureIsAdmin::class,
+            'isAdminOrApprover' => AdminOrApproverMiddleware::class
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
